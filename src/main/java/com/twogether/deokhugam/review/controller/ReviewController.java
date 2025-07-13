e5f60af10bc6f555d913eb0f7df47ae500daaff4
wangcoJiin <mf0d070208@gmail.com>
2025-07-14 00:28:12 +0900

    feat: 리뷰 상세 조회 컨트롤러 메서드 추가

--- src/main/java/com/twogether/deokhugam/review/controller/ReviewController.java
+++ src/main/java/com/twogether/deokhugam/review/controller/ReviewController.java
@@ -2,13 +2,18 @@ package com.twogether.deokhugam.review.controller;
 
 import com.twogether.deokhugam.review.dto.ReviewDto;
 import com.twogether.deokhugam.review.dto.request.ReviewCreateRequest;
+import com.twogether.deokhugam.review.exception.ReviewNotFoundException;
 import com.twogether.deokhugam.review.service.ReviewService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +34,19 @@ public class ReviewController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
+                .body(reviewDto);
+    }
+
+    // 리뷰 상세 조회
+    @GetMapping(path = "/{reviewId}")
+    public ResponseEntity<ReviewDto> findById(
+            @PathVariable("reviewId") UUID reviewId,
+            @RequestHeader(value = "Deokhugam-Request-User-ID", required = true) UUID requestUserId
+    ){
+        ReviewDto reviewDto = reviewService.findById(reviewId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(reviewDto);
     }
 }
