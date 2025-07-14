7d932f4fc4692299ece028aee4dc006b27e7caa9
okodee <okodee@naver.com>
2025-07-14 13:48:27 +0900

    feat: JPA Auditing Configuration 등록

---
+++ src/main/java/com/twogether/deokhugam/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.twogether.deokhugam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+
+}
