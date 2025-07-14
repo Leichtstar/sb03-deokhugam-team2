c603c02e491114cec10fede3154e6568ade9c177
wangcoJiin <mf0d070208@gmail.com>
2025-07-14 13:33:55 +0900

    feat: ErrorCode에 REVIEW_LIKE_NOT_FOUND 추가

--- src/main/java/com/twogether/deokhugam/common/exception/ErrorCode.java
+++ src/main/java/com/twogether/deokhugam/common/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
     REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 작성한 리뷰가 있습니다."),
-    INVALID_RATING(HttpStatus.BAD_REQUEST, "리뷰 평점은 1점 이상 5점 이하이어야 합니다.");
+    INVALID_RATING(HttpStatus.BAD_REQUEST, "리뷰 평점은 1점 이상 5점 이하이어야 합니다."),
+
+    REVIEW_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰 좋아요 정보를 찾을 수 없습니다.");
 
     private final String message;
     private final HttpStatus status;
