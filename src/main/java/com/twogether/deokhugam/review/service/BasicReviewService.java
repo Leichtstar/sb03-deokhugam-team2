2b7632c509095e7b2533add12a4b6f43c8a18d1d
wangcoJiin <mf0d070208@gmail.com>
2025-07-14 13:26:51 +0900

    feat: 리뷰 생성시 리뷰 좋아요 정보도 생성하도록 수정, 리뷰 좋아요 설정 메서드 추가

--- src/main/java/com/twogether/deokhugam/review/service/BasicReviewService.java
+++ src/main/java/com/twogether/deokhugam/review/service/BasicReviewService.java
@@ -3,11 +3,16 @@ package com.twogether.deokhugam.review.service;
 import com.twogether.deokhugam.book.entity.Book;
 import com.twogether.deokhugam.book.repository.BookRepository;
 import com.twogether.deokhugam.review.dto.ReviewDto;
+import com.twogether.deokhugam.review.dto.ReviewLikeDto;
 import com.twogether.deokhugam.review.dto.request.ReviewCreateRequest;
 import com.twogether.deokhugam.review.entity.Review;
+import com.twogether.deokhugam.review.entity.ReviewLike;
 import com.twogether.deokhugam.review.exception.ReviewExistException;
+import com.twogether.deokhugam.review.exception.ReviewLikeNotFoundException;
 import com.twogether.deokhugam.review.exception.ReviewNotFoundException;
+import com.twogether.deokhugam.review.mapper.ReviewLikeMapper;
 import com.twogether.deokhugam.review.mapper.ReviewMapper;
+import com.twogether.deokhugam.review.repository.ReviewLikeRepository;
 import com.twogether.deokhugam.review.repository.ReviewRepository;
 import com.twogether.deokhugam.user.entity.User;
 import com.twogether.deokhugam.user.repository.UserRepository;
@@ -26,7 +31,9 @@ public class BasicReviewService implements ReviewService{
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
     private final BookRepository bookRepository;
+    private final ReviewLikeRepository reviewLikeRepository;
     private final ReviewMapper reviewMapper;
+    private final ReviewLikeMapper reviewLikeMapper;
 
     // 리뷰 생성
     @Override
@@ -49,6 +56,13 @@ public class BasicReviewService implements ReviewService{
         Review review = new Review(reviewdBook, reviewer, request.content(), request.rating());
         reviewRepository.save(review);
 
+        ReviewLike reviewLike = new ReviewLike(
+                review,
+                reviewer,
+                false
+        );
+        reviewLikeRepository.save(reviewLike);
+
         log.info("[BasicReviewService] 리뷰 등록 성공");
 
         return reviewMapper.toDto(review);
@@ -62,7 +76,33 @@ public class BasicReviewService implements ReviewService{
                 .orElseThrow(
                         () -> new ReviewNotFoundException(reviewId));
 
+        log.info("리뷰 조회 완료");
+
         return reviewMapper.toDto(review);
     }
 
+    // 리뷰 좋아요 기능
+    @Override
+    public ReviewLikeDto reviewLike(UUID reviewId, UUID userId) {
+        ReviewLike reviewLike = reviewLikeRepository.findByUserIdAndReviewId(userId, reviewId)
+                .orElseThrow(() -> new ReviewLikeNotFoundException());
+
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewNotFoundException(reviewId));
+
+        if (reviewLike.isLiked()){
+            // 좋아요 상태가 true 라면
+            reviewLike.updateLike(false);
+            review.updateLikeCount(review.getLikeCount() - 1);
+        }
+        else{
+            // 좋아요가 false 라면
+            reviewLike.updateLike(true);
+            review.updateLikeCount(review.getLikeCount() + 1);
+        }
+        reviewLikeRepository.save(reviewLike);
+        reviewRepository.save(review);
+
+        return reviewLikeMapper.toDto(reviewLike);
+    }
 }
