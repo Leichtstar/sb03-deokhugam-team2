a71103454e668b49d0763ebbe56ef4b49589b4f6
noonsong0208 <tmdwls7795@gmail.com>
2025-07-16 13:08:08 +0900

    chore: QueryDSL 설정 및 엔티티 정리

--- src/main/java/com/twogether/deokhugam/comments/entity/Comment.java
+++ src/main/java/com/twogether/deokhugam/comments/entity/Comment.java
@@ -1,15 +1,15 @@
 package com.twogether.deokhugam.comments.entity;
 
-import com.twogether.deokhugam.review.entity.Review;
-import com.twogether.deokhugam.user.entity.User;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import com.twogether.deokhugam.review.entity.Review;
+import com.twogether.deokhugam.user.entity.User;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
