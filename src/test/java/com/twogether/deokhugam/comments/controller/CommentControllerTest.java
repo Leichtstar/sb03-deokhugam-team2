d7db64bbc57fc03f60d2cb22e825c8cdd1d7bacd
noonsong0208 <tmdwls7795@gmail.com>
2025-07-17 15:48:45 +0900

    feat: 댓글 상세 조회 테스트코드 작성

--- src/test/java/com/twogether/deokhugam/comments/controller/CommentControllerTest.java
+++ src/test/java/com/twogether/deokhugam/comments/controller/CommentControllerTest.java
@@ -5,13 +5,16 @@ import com.twogether.deokhugam.comments.service.CommentQueryService;
 import com.twogether.deokhugam.comments.service.CommentService;
 
 import com.twogether.deokhugam.common.dto.CursorPageResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -44,4 +47,43 @@ class CommentControllerTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.content").isArray());
     }
+
+    @Test
+    @DisplayName("댓글 단건 조회 성공")
+    void getComment_success() throws Exception {
+        UUID id = UUID.randomUUID();
+        String content = "테스트";
+        UUID userId = UUID.randomUUID();
+        String userNickname = "user1";
+        UUID reviewId = UUID.randomUUID();
+        LocalDateTime createdAt = LocalDateTime.now();
+        LocalDateTime updatedAt = LocalDateTime.now();
+        Boolean isDeleted = false;
+
+        CommentResponse dto = new CommentResponse(
+            id, content, userId, userNickname, reviewId, createdAt, updatedAt, isDeleted
+        );
+
+        when(commentService.getComment(id)).thenReturn(dto);
+
+        mockMvc.perform(get("/api/comments/{id}", id))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(id.toString()))
+            .andExpect(jsonPath("$.content").value(content))
+            .andExpect(jsonPath("$.userId").value(userId.toString()))
+            .andExpect(jsonPath("$.userNickname").value(userNickname))
+            .andExpect(jsonPath("$.reviewId").value(reviewId.toString()))
+            .andExpect(jsonPath("$.isDeleted").value(isDeleted));
+    }
+
+    @Test
+    @DisplayName("댓글 단건 조회 시 NotFound 예외")
+    void getComment_notFound() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(commentService.getComment(id)).thenThrow(new NoSuchElementException("댓글이 없습니다."));
+
+        mockMvc.perform(get("/api/comments/{id}", id))
+            .andExpect(status().isNotFound());
+    }
+
 }
