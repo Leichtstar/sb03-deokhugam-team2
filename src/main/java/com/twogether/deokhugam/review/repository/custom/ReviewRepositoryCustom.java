06c86088ac1f11dc6be540eabf8ff8c050f6dba6
wangcoJiin <mf0d070208@gmail.com>
2025-07-17 10:18:15 +0900

    fix: ReviewRepositoryCustom 충돌 해결을 위해 dashboard 관련 메서드 삭제

--- src/main/java/com/twogether/deokhugam/review/repository/custom/ReviewRepositoryCustom.java
+++ src/main/java/com/twogether/deokhugam/review/repository/custom/ReviewRepositoryCustom.java
@@ -1,16 +1,11 @@
 package com.twogether.deokhugam.review.repository.custom;
 
-import com.twogether.deokhugam.dashboard.batch.model.BookScoreDto;
 import com.twogether.deokhugam.review.dto.request.ReviewSearchRequest;
 import com.twogether.deokhugam.review.entity.Review;
-import java.time.LocalDateTime;
-import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface ReviewRepositoryCustom {
-    List<BookScoreDto> calculateBookScores(LocalDateTime start, LocalDateTime end);
-    List<BookScoreDto> calculateBookScoresAllTime();
 
     long totalElementCount(ReviewSearchRequest reviewSearchRequest);
     Slice<Review> findReviewsWithCursor(ReviewSearchRequest request, Pageable pageable);
