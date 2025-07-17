495f80ce15bbb2f4412e194c5b9c4509d8940ae6
bs8841 <49220565+bs8841@users.noreply.github.com>
2025-07-17 10:13:37 +0900

    ReviewRepositoryCustom 충돌 해결.java

--- src/main/java/com/twogether/deokhugam/review/repository/custom/ReviewRepositoryCustom.java
+++ src/main/java/com/twogether/deokhugam/review/repository/custom/ReviewRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.twogether.deokhugam.review.repository.custom;
 
-import com.twogether.deokhugam.dashboard.batch.model.BookScoreDto;
 import com.twogether.deokhugam.review.dto.request.ReviewSearchRequest;
 import com.twogether.deokhugam.review.entity.Review;
 import java.time.LocalDateTime;
@@ -9,8 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface ReviewRepositoryCustom {
-    List<BookScoreDto> calculateBookScores(LocalDateTime start, LocalDateTime end);
-    List<BookScoreDto> calculateBookScoresAllTime();
 
     long totalElementCount(ReviewSearchRequest reviewSearchRequest);
     Slice<Review> findReviewsWithCursor(ReviewSearchRequest request, Pageable pageable);
