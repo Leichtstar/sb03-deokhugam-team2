c603d4833b068ea3dc5d377c590881a969497bcc
noonsong0208 <tmdwls7795@gmail.com>
2025-07-17 16:16:08 +0900

    feat: ReviewRepositoryCustom 확장

--- src/main/java/com/twogether/deokhugam/review/repository/ReviewRepository.java
+++ src/main/java/com/twogether/deokhugam/review/repository/ReviewRepository.java
@@ -2,11 +2,13 @@ package com.twogether.deokhugam.review.repository;
 
 import com.twogether.deokhugam.review.entity.Review;
 import java.util.UUID;
+
+import com.twogether.deokhugam.review.repository.custom.ReviewRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
+public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRepositoryCustom {
 
     boolean existsByUserIdAndBookId(UUID userId, UUID bookId);
 
