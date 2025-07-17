7fbf60f9433fc3a05a7d0753d1a2975f689e07ff
bs8841 <whqortjsgkgk@naver.com>
2025-07-17 13:09:37 +0900

    feat: 인기 리뷰 랭킹 레포지토리 생성

---
+++ src/main/java/com/twogether/deokhugam/dashboard/repository/PopularReviewRankingRepository.java
@@ -0,0 +1,12 @@
+package com.twogether.deokhugam.dashboard.repository;
+
+import com.twogether.deokhugam.dashboard.entity.PopularReviewRanking;
+import com.twogether.deokhugam.dashboard.entity.RankingPeriod;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopularReviewRankingRepository extends JpaRepository<PopularReviewRanking, UUID> {
+
+    List<PopularReviewRanking> findByPeriodOrderByRankAsc(RankingPeriod period);
+}
