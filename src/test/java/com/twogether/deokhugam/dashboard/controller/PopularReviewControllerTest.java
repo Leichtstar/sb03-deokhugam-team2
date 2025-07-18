1890857c2ad6b76ab09d018e6ca5fd036e2a4be3
bs8841 <whqortjsgkgk@naver.com>
2025-07-18 17:05:04 +0900

    test: 인기 도서 및 리뷰 컨트롤러 테스트 대상 클래스 수정

--- src/test/java/com/twogether/deokhugam/dashboard/controller/PopularReviewControllerTest.java
+++ src/test/java/com/twogether/deokhugam/dashboard/controller/PopularReviewControllerTest.java
@@ -20,7 +20,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(DashboardController.class)
+@WebMvcTest(PopularReviewControllerTest.class)
 class PopularReviewControllerTest {
 
     @Autowired
