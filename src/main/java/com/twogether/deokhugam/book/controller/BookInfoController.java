b277f61006f49b4a984fa32f09f6201fefd3b153
Leichtstar <leichtnidel@gmail.com>
2025-07-21 15:05:46 +0900

    fix: Ocr 인식 컨트롤러 Get->Post로 변경

--- src/main/java/com/twogether/deokhugam/book/controller/BookInfoController.java
+++ src/main/java/com/twogether/deokhugam/book/controller/BookInfoController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -32,8 +33,8 @@ public class BookInfoController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
-    @GetMapping(value = "/isbn/ocr")
-    public ResponseEntity<String> getIsbnFromOcr(@RequestPart(value = "image", required = false) MultipartFile bookImage){
+    @PostMapping(value = "/isbn/ocr")
+    public ResponseEntity<String> getIsbnFromOcr(@RequestPart(value = "image", required = true) MultipartFile bookImage){
     if (bookImage.isEmpty() || bookImage.getSize() == 0){
         throw new IllegalArgumentException("인식할 이미지가 없습니다.");
     }
