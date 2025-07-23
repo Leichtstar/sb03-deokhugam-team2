4020b87e2c5245e5113845b8db0e40c63ada6878
bs8841 <whqortjsgkgk@naver.com>
2025-07-23 19:03:41 +0900

    refactor: 대시보드 엔티티의 createdAt 필드를 Instant로 변경

--- src/main/java/com/twogether/deokhugam/dashboard/entity/PopularReviewRanking.java
+++ src/main/java/com/twogether/deokhugam/dashboard/entity/PopularReviewRanking.java
@@ -1,25 +1,18 @@
 package com.twogether.deokhugam.dashboard.entity;
 
-import com.twogether.deokhugam.book.entity.Book;
-import com.twogether.deokhugam.review.entity.Review;
-import com.twogether.deokhugam.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -97,7 +90,7 @@ public class PopularReviewRanking {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     public void assignRank(int rank) {
         this.rank = rank;
