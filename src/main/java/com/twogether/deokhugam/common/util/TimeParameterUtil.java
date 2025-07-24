011c535c377e99026b3ced94683385ccacba28d6
bs8841 <whqortjsgkgk@naver.com>
2025-07-24 11:57:12 +0900

    feat: parse 예외 처리를 위한 util 클래스 생성

---
+++ src/main/java/com/twogether/deokhugam/common/util/TimeParameterUtil.java
@@ -0,0 +1,18 @@
+package com.twogether.deokhugam.common.util;
+
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeParameterUtil {
+
+    public static Instant parseNowOrDefault(String nowString) {
+        try {
+            return Instant.parse(nowString);
+        } catch (DateTimeParseException | NullPointerException e) {
+            log.warn("[TimeParameterUtil] 'now' 파라미터 파싱 실패. Instant.now()를 사용합니다. nowString={}", nowString, e);
+            return Instant.now();
+        }
+    }
+}
