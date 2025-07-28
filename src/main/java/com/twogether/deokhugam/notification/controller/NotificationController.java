af113123c07170493c72c089d37ee0b9940a454c
bs8841 <whqortjsgkgk@naver.com>
2025-07-28 18:58:19 +0900

    refactor: Notification Controller Tag 적용

--- src/main/java/com/twogether/deokhugam/notification/controller/NotificationController.java
+++ src/main/java/com/twogether/deokhugam/notification/controller/NotificationController.java
@@ -7,6 +7,7 @@ import com.twogether.deokhugam.notification.service.NotificationQueryService;
 import com.twogether.deokhugam.notification.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import java.time.Instant;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "알림 관리", description = "알림 관련 API")
 @RestController
 @RequiredArgsConstructor
 @Validated
